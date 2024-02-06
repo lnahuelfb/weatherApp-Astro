6edc8bac3b7982f1f2773ffa0a295d614429941e
LNahuelFB <lnahuelfernandezb@gmail.com>
2024-02-06 00:27:40 -0300

    Aplicado el responsive y terminado el header

---
+++ src/components/react/Weather.tsx
@@ -0,0 +1,10 @@
+import Card from "../WeatherCard.astro";
+import Cloudy from "../icons/Cloudy.astro";
+
+export default function Weather() {
+  return (
+    <article class='bg-[#34495E]/20 w-full h-60 rounded-2xl '>
+      <img src='/favicon.svg' class="size-6" />
+    </article>
+  )
+}
---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    screens: {
+      'sm': '576px',
+      'md': '960px',
+      'lg': '1440px',
+    },
+    extend: {}
+  },
+  plugins: [],
+}
